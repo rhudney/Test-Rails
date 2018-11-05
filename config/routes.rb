497320c92ba2f54e89e0d2fd22dc3955b4421bf8
Rhudney <rhudney.simoes@gmail.com>
2018-11-05 15:18:49 +0000

    Config the routes file for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # www.meusite.com.br/users
+  # namespace :api, defaults: {format: :json}, path "/" do
+
+  # www.meusite.com.br/api/users
+  # namespace :api, defaults: {format: :json} do
+
+  # api.meusite.com.br/users
+  namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path "/" do
+
+  end
+
 end
